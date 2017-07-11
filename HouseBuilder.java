ef20032b4304924617e69c51692eb12d5eea554b
delficaffa <md.caffa@hotmail.com>
2017-07-11 10:35:52 -0300

    Add files via upload

---
+++ HouseBuilder.java
@@ -0,0 +1,17 @@
+
+public interface HouseBuilder {
+
+	buildarea();
+	
+	buildkitchen();
+	
+	buildlivingroom();
+	
+	builddinningroom();
+	
+	buildbedroom();
+
+	buildbathroom();
+	
+	House getHouse();	
+}
